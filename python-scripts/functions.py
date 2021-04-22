68c2a98bb0d43c162c975bb2484a3617ccdc1550
ebauman77 <e.baumann77@gmail.com>
2021-04-22 10:04:26 +0200

    changed notebooks for new movie structure

--- python-scripts/functions.py
+++ python-scripts/functions.py
@@ -1,14 +1,45 @@
+import h5py
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.ndimage
+import os
 import pandas as pd
+import imageio
+from skimage import io
+from skimage.filters import threshold_otsu
+from scipy.ndimage import gaussian_filter
+import matplotlib
+import yaml
+
+font = {'family': 'normal', 'weight' : 'bold', 'size' : 22}
+matplotlib.rc('font', **font)
+
+def read_parameters():
+    parameters = dict()
+
+    with open("base/parameters.yml") as file:
+        parameters = yaml.load(file, Loader=yaml.FullLoader)
+    with open("local/parameters.yml") as file:
+        parameters_local = yaml.load(file, Loader=yaml.FullLoader)
+
+    # overwrite global parameters with local setting
+    for key in parameters_local:
+        parameters[key] = parameters_local[key]
+
+    return parameters
 
 
 def read_key_file(parameters):
-    
-    
-    key_file = pd.DataFrame()
-    
-    
+    file_path = parameters['data_folder'] + "00_Extra/Details_imaging_data.xls"
+
+    key_file = pd.read_excel(file_path)
+
     return key_file
 
+#def read_key_file(parameters):
+ #   key_file = pd.DataFrame()
+  #  return key_file
 
 
 
+def calculate_tumor_macrophage_distance_3D(parameters, key_file):
--- python-scripts/run_3D.py
+++ python-scripts/run_3D.py
@@ -1,37 +1,28 @@
-import pandas as pd
-import yaml
+import functions
 
-def read_parameters():
-
-    parameters = dict()
-
-    with open("../base/parameters.yml") as file:
-        parameters = yaml.load(file, Loader=yaml.FullLoader)
-    with open("../local/parameters.yml") as file:
-        parameters_local = yaml.load(file, Loader=yaml.FullLoader)
-
-    # overwrite global parameters with local setting
-    for key in parameters_local:
-        parameters[key] = parameters_local[key]
-    
-    return parameters
-
-
-def read_key_file(parameters):
-    
-    file_path = parameters['data_folder'] + "00_Extra/Details_imaging_data.xlsx"
-
-    key_file = pd.read_excel(file_path)
-        
-    return key_file
-
-parameters = read_parameters()
+parameters = functions.read_parameters()
 print("#"*5,"parameters","#"*5)
 print(parameters)
-key_file = read_key_file(parameters)
+key_file = functions.read_key_file(parameters)
 print("#"*5,"key_file","#"*5)
 print(key_file.head())
 
+#======================================================================================================================
+#=                                            STEP 1: COMPUTE ALL RESULTS                                             =
+#======================================================================================================================
 
 
+
+
+#======================================================================================================================
+#=                                              STEP 2: LOAD RESULTS                                                  =
+#======================================================================================================================
+
+
+
+
+
+#======================================================================================================================
+#=                              STEP 3: PLOT RESULTS FOR DIFFERENT EXPERIMENTAL CONDITIONS                            =
+#======================================================================================================================
 
