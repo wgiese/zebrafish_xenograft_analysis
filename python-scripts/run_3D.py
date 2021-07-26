937e5a3fa08ec32b2efc07a7f9f52cf85025592e
wgiese <wolfgang.giese@mdc-berlin.de>
2021-07-26 18:10:42 +0200

    -

--- python-scripts/run_3D.py
+++ python-scripts/run_3D.py
@@ -10,7 +10,8 @@ print(key_file.head())
 print(key_file.keys())
 
 
-macrophage_properties_df = functions_3D.get_macrophage_properties(parameters, key_file, experiment ="200804_LBT070_1dpi_Pos001", npy_out = True, vtk_out = True)
+#macrophage_properties_df = functions_3D.get_macrophage_properties(parameters, key_file, experiment ="200804_LBT070_1dpi_Pos001", npy_out = True, vtk_out = True)
+macrophage_properties_df = functions_3D.get_macrophage_properties(parameters, key_file, experiment ="all", npy_out = True, vtk_out = True)
 
 print(macrophage_properties_df)
 
