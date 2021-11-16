cc535d4c582b39c97c8c9fa25627d8b9f5882d10
wgiese <wolfgang.giese@mdc-berlin.de>
2021-11-16 21:40:58 +0100

    added run_cellpose_2D.py

--- python-scripts/run_cellpose_2D.py
+++ python-scripts/run_cellpose_2D.py
@@ -27,6 +27,52 @@ print("#"*5,"key_file","#"*5)
 print(key_file.head())
 
 
+data_path = parameters["data_folder"]
+folder_2d_data = "/03_Preprocessed_Data/01_2D/"
+use_gpu = parameters["use_gpu"]
+output_folder = data_path + "/cellpose_segmentation/"
+
+
+for index, row in key_file.iterrows():
+
+    
+    file_path = data_path + folder_2d_data + str(row["short_name"]) + ".tif"
+    
+    if not os.path.exists(file_path):
+        continue
+
+    print("open image file %s" % file_path)
+        
+    img = skimage.io.imread(file_path)
+    
+    print(img.shape)
+   
+    #for time in range(img.shape[0]):
+    #    print(time)
+    #    macrophage_img = img[time,:,:,parameters["channel_macrophages"]]
+
+    time = 0
+    macrophage_img = img[time,:,:,parameters["channel_macrophages"]]
+    
+    channels = [0,0]
+    model = models.Cellpose(gpu=use_gpu, model_type='cyto')
+    masks, flows, styles, diams = model.eval(macrophage_img, diameter=25, channels=channels)
+
+    fig, ax = plt.subplots(figsize=(15,15))
+    ax.imshow(macrophage_img, cm.binary)
+    ax.imshow(masks, cm.Set3, alpha = 0.5)
+    plt.savefig(output_folder + str(row["short_name"]) + "-%s.png" % time)
+    
+        
+    
+# choice for greyscale
+#channels = [0,0]
+
+#masks, flows, styles, diams = model.eval(img, diameter=40, channels=channels)
+
+#io.masks_flows_to_seg(img , masks, flows, diams, output_filepath , channels)
+
+
 
 #filename = parameters['image_path']
 ### remove later
